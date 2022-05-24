d67b615caa36a91b48d45eceb494c99938e046f4
Jamie Harris <jamiedodgerification12345@gmail.com>
2022-05-24 21:36:56 +0100

    Completed "Bank Heist" project.

--- _3_conditionals/Project - Bank Heist.go
+++ _3_conditionals/Project - Bank Heist.go
@@ -1,5 +1,59 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	isHeistOn := true
+	eludedGuards := rand.Intn(100)
+
+	if eludedGuards >= 50 {
+		fmt.Println("Looks like you've managed to make it past the guards. Good job, but remember, this is the first step.")
+	} else {
+		isHeistOn = false
+		fmt.Println("Plan a better disguise next time?")
+	}
+
+	openedVault := rand.Intn(100)
+
+	if isHeistOn && openedVault >= 70 {
+		fmt.Println("Grab and GO!")
+	} else if isHeistOn {
+		isHeistOn = false
+		fmt.Println("The vault can't be opened.")
+	}
+
+	leftSafety := rand.Intn(5)
+
+	if isHeistOn {
+		switch leftSafety {
+		case 0:
+			isHeistOn = false
+			fmt.Println("The heist failed.")
+		case 1:
+			isHeistOn = false
+			fmt.Println("Fell into a cell...")
+		case 2:
+			isHeistOn = false
+			fmt.Println("Hyperinflation caused the money to be worthless...")
+		case 3:
+			isHeistOn = false
+			fmt.Println("Walked into a black hole...")
+		default:
+			fmt.Println("Start the getaway car!")
+		}
+
+		if isHeistOn {
+			amtStolen := 10000 + rand.Intn(1000000)
+
+			fmt.Println("Successfully stolen:", amtStolen)
+		}
+	}
+
+	fmt.Println("isHeistOn:", isHeistOn)
 }
