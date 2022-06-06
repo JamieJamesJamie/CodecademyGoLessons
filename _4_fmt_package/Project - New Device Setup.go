4b77e9c88ceadf2b7d027bb99e0a7d6397aa4cee
Jamie Harris <jamiedodgerification12345@gmail.com>
2022-06-06 14:15:14 +0100

    Completed "New Device Setup" project.

--- _4_fmt_package/Project - New Device Setup.go
+++ _4_fmt_package/Project - New Device Setup.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var name string
+
+	fmt.Println("What is your name?")
+	fmt.Scan(&name)
+	fmt.Println("Hello", name, "!")
+
+	var age int
+
+	fmt.Scan(&age)
+	fmt.Printf("Hello %s! You are %d years old.\n", name, age)
+
+	newMessage := fmt.Sprintf("Wow %s, you're %d years old!", name, age)
+	fmt.Println(newMessage)
 }
