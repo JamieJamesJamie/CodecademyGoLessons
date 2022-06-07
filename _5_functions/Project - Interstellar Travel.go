b713bb659f0c1feb6e7d9b5fc71fa12ba0007e7f
Jamie Harris <jamiedodgerification12345@gmail.com>
2022-06-07 16:42:04 +0100

    Pasted "Interstellar Travel" template from Codecademy.

--- _5_functions/Project - Interstellar Travel.go
+++ _5_functions/Project - Interstellar Travel.go
@@ -1,5 +1,20 @@
 package main
 
+// Create the function fuelGauge() here
+
+// Create the function calculateFuel() here
+
+// Create the function greetPlanet() here
+
+// Create the function cantFly() here
+
+// Create the function flyToPlanet() here
+
 func main() {
+	// Test your functions!
+
+	// Create `planetChoice` and `fuel`
+
+	// And then liftoff!
 
 }
