52da82ad08d82bd8b34ddcb8b43052654a9ca221
Jamie Harris <jamiedodgerification12345@gmail.com>
2022-06-09 15:57:39 +0100

    Pasted "Looping through a Food Order" template from Codecademy.

--- _learn_go_-_loops_arrays_maps_and_structs/_1_loops/Project - Looping through a Food Order.go
+++ _learn_go_-_loops_arrays_maps_and_structs/_1_loops/Project - Looping through a Food Order.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func askOrder() string {
+	var input string
+	fmt.Print("What would you like to eat: ")
+	// Get the input from the user
+	fmt.Scan(&input)
+	return input
+}
+
+// WRITE CONTAINS FUNCTION BELOW
+
+func main() {
+	fastfoodMenu := []string{"Burgers", "Nuggets", "Fries"}
+	fmt.Println("The fast food menu has these items:", fastfoodMenu)
+
+	var total int
+	var order string
+	// WRITE INDEFINITE LOOP ASKING FOR ORDERS BELOW
+
+	// WRITE DEFINITE LOOP COUNTING $2 BILLS BELOW
+
+	fmt.Println("The total for the order is", total)
 }
