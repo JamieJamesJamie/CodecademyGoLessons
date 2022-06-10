ab2bd5e5a19d27129461487e1d77f416d36a3e5d
Jamie Harris <jamiedodgerification12345@gmail.com>
2022-06-10 14:33:22 +0100

    The house has been set up with guests and cat objects.

--- _learn_go_-_loops_arrays_maps_and_structs/_2_arrays/Project - Catnapped.go
+++ _learn_go_-_loops_arrays_maps_and_structs/_2_arrays/Project - Catnapped.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	guests := [4]string{"Person1", "Person2", "Person3", "Person4"}
+	catObjects := [3]string{"A Toy Chest", "A Crate", "A Box"}
+
+	fmt.Println("Guests:", guests)
+	fmt.Println("Cat's objects:", catObjects)
 }
