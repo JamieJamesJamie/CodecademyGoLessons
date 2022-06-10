52a8371b73f608a69162652bdea6af4dce95949d
Jamie Harris <jamiedodgerification12345@gmail.com>
2022-06-10 14:46:50 +0100

    Generated a random perpetrator and cat object to be used for the catnapped project.

--- _learn_go_-_loops_arrays_maps_and_structs/_2_arrays/Project - Catnapped.go
+++ _learn_go_-_loops_arrays_maps_and_structs/_2_arrays/Project - Catnapped.go
@@ -13,10 +13,15 @@ func getRandomElement(slice []string) string {
 
 func main() {
 	guests := [4]string{"Person1", "Person2", "Person3", "Person4"}
-	catObjects := [3]string{"A Toy Chest", "A Crate", "A Box"}
+	catObjects := [3]string{"Toy Chest", "Crate", "Box"}
 
 	fmt.Println("Guests:", guests)
 	fmt.Println("Cat's objects:", catObjects)
 
 	rand.Seed(time.Now().UnixNano())
+
+	randomGuest := getRandomElement(guests[:])
+	randomCatObject := getRandomElement(catObjects[:])
+
+	fmt.Println(randomGuest, "hid the cat by putting it in the", randomCatObject)
 }
