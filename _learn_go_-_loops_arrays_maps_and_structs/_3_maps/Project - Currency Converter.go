7ce74c12daaa9ca22687a71bd3008f7e588b2f5d
Jamie Harris <jamiedodgerification12345@gmail.com>
2022-06-10 15:51:06 +0100

    Completed the "Currency Converter" project.

--- _learn_go_-_loops_arrays_maps_and_structs/_3_maps/Project - Currency Converter.go
+++ _learn_go_-_loops_arrays_maps_and_structs/_3_maps/Project - Currency Converter.go
@@ -1,6 +1,32 @@
 package main
 
+import fmt "fmt"
+
 func main() {
 	currencies := map[string]float32{"JPY": 130.2, "EUR": 0.95}
 
+	var dollarAmount float32
+	var currency string
+
+	fmt.Println("How many dollars (USD) would you like to convert?")
+	fmt.Print("$")
+	fmt.Scan(&dollarAmount)
+
+	if dollarAmount == 0 {
+		fmt.Println("Invalid stick price.")
+	} else {
+		fmt.Println("What is the target currency?")
+		fmt.Scan(&currency)
+
+		rate, isValid := currencies[currency]
+
+		if !isValid {
+			fmt.Printf("\"%s\" is not a valid currency.", currency)
+			fmt.Println()
+			return
+		}
+
+		fmt.Printf("%.2f (USD) has been converted to %.2f (%s)", dollarAmount, dollarAmount*rate, currency)
+		fmt.Println()
+	}
 }
