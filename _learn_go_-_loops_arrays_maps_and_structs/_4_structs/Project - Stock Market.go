dc3418ae6e4fbecbe78764f445851394f9f04c4d
Jamie Harris <jamiedodgerification12345@gmail.com>
2022-06-11 14:41:24 +0100

    Added Codecademy template for "Stock Market" project file.

--- _learn_go_-_loops_arrays_maps_and_structs/_4_structs/Project - Stock Market.go
+++ _learn_go_-_loops_arrays_maps_and_structs/_4_structs/Project - Stock Market.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"math/rand"
+	"time"
+)
 
+func randomNumberGen(min float32, max float32) float32 {
+	return min + (max-min)*rand.Float32()
+}
+
+// Task implementation goes here
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	// Function calls go here
 }
