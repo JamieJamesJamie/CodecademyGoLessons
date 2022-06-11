b4a34fd51e64652f52ad232d31cf0a4c9847f44b
Jamie Harris <jamiedodgerification12345@gmail.com>
2022-06-11 15:07:46 +0100

    Completed the "Stock Market" project.

--- _learn_go_-_loops_arrays_maps_and_structs/_4_structs/Project - Stock Market.go
+++ _learn_go_-_loops_arrays_maps_and_structs/_4_structs/Project - Stock Market.go
@@ -31,4 +31,20 @@ func displayMarket(market []Stock) {
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	// Function calls go here
+
+	stockMarket := []Stock{
+		{name: "GOOG", price: 2313.50},
+		{name: "AAPL", price: 157.28},
+		{name: "FB", price: 203.77},
+		{name: "TWTR", price: 50.00},
+	}
+
+	fmt.Println("Current state of the stock market:")
+	displayMarket(stockMarket)
+	fmt.Println()
+
+	stockMarket[1].updateStock()
+
+	fmt.Println(fmt.Sprintf("Updated state of the stock market (the price of %s has been updated):", stockMarket[1].name))
+	displayMarket(stockMarket)
 }
