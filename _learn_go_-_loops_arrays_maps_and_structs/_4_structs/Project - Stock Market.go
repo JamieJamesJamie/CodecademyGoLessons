927e0e285df17ab9a6d6d65057181fecd2eb5529
Jamie Harris <jamiedodgerification12345@gmail.com>
2022-06-11 14:45:24 +0100

    Implemented "Stock" struct.

--- _learn_go_-_loops_arrays_maps_and_structs/_4_structs/Project - Stock Market.go
+++ _learn_go_-_loops_arrays_maps_and_structs/_4_structs/Project - Stock Market.go
@@ -11,6 +11,11 @@ func randomNumberGen(min float32, max float32) float32 {
 
 // Task implementation goes here
 
+type Stock struct {
+	name  string
+	price float32
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	// Function calls go here
